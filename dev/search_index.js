e8c0ecfae151f34b8eb9b49311f5ca2c990e32d4
Documenter.jl <documenter@juliadocs.github.io>
2026-01-03 21:03:11 +0000

    build based on ff02700

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Hephaestus","page":"Home","title":"Hephaestus","text":"Documentation for Hephaestus.\n\n","category":"section"}]
+[{"location":"#Hephaestus","page":"Home","title":"Hephaestus","text":"Documentation for Hephaestus.\n\n","category":"section"},{"location":"#Hephaestus.generate_tiling_svg-Tuple{AbstractString}","page":"Home","title":"Hephaestus.generate_tiling_svg","text":"generate_tiling_svg(output_path; kwargs...)\n\nGenerate a de Bruijn multigrid quasiperiodic tiling SVG based on the Pattern Collider web version logic.\n\n\n\n\n\n","category":"method"}]
 }
